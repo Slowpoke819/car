63dc3f0e2e294bec3c4016a50a937be4978b68d5
Slowpoke819 <ivanovdk25@ptk.ithub.ru>
2025-11-24 14:54:56 +1200

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 let car = document.querySelector('.car');
+let scene = document.querySelector('.scene');
 let carWidth = 200;
-let stop = window.innerWidth - carWidth - 50;
+let stop = scene.clientWidth - carWidth - 50;
 let pos = -carWidth;
-let move = setInterval(function() {
-  if (pos < stop) {
-    pos += 4;
-    car.style.left = pos + "px";
-  } else {
-    clearInterval(move);
-  }
-}, 10);
+        let move = setInterval(() => {
+            if (pos < stop) {
+                pos += 3;
+                car.style.left = pos + 'px';
+            } else {
+                clearInterval(move);
+            }
+        }, 10);
